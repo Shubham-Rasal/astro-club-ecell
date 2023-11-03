048639c6f93c8085a08c00391be53ad9debe6c5f
Shubham-Rasal <bluequbits@gmail.com>
2023-11-03 14:34:14 +0530

    added icons

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
   links: [
-    
+
     {
       text: 'About',
       links: [
@@ -43,31 +43,24 @@ export const footerData = {
       title: 'Explore',
       links: [
         { text: 'Home', href: '#' },
-        { text: 'About Us', href: '#' },
-        { text: 'Blog', href: '#' },  
-          
+        { text: 'About Us', href: '/about' },
+        { text: 'Blog', href: '/blog' },
+
       ],
     },
     {
       title: 'Initiatives',
       links: [
-        { text: 'SIP', href: '#' },
-        { text: 'CAP', href: '#' },
         { text: 'Podcast', href: '#' },
-        { text: 'Startup Hub', href: '#' },
-        { text: 'E Summit', href: '#' },
+      { text: 'E Summit', href: '#' },
       ],
     },
     {
       title: 'Feedback',
       links: [
-        {text: 'Contact', href: '#'}           
+        { text: 'Contact', href: '#' }
       ],
     },
-  ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
     { ariaLabel: 'Twitter', icon: 'tabler:brand-twitter', href: '#' },
@@ -76,5 +69,5 @@ export const footerData = {
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
-  
+
 };
